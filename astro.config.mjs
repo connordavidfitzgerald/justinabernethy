417023f591a4e7c5524c94b71ae532055ada55c9
connor <hello@connordavidfitzgerald.com>
2026-04-07 17:27:32 -0400

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import lenis from "astro-lenis";
+
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Custom",
+      cssVariable: "--font-sans",
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: "normal",
+            src: ["./src/assets/fonts/AntiqueLegacy-Book.otf"],
+            display: "block",
+          },
+          // ...
+        ],
+      },
+    },
+  ],
+
+  integrations: [
+    lenis(),
+    sanity({
+      projectId: "t3fe0x9u",
+      dataset: "production",
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      apiVersion: "2026-04-01",
+    }),
+  ],
+});
